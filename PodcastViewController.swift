b85a2b4602c0f4896247762f11e6723785f5cbb9
Mitch Guzman <mguzman9294@gmail.com>
2017-03-18 17:24:22 -0700

    Getting podcast URL’s to be added into core data

--- PodcastViewController.swift
+++ PodcastViewController.swift
@@ -8,12 +8,38 @@
 
 import Cocoa
 
-class PodcastViewController: NSViewController {
+class PodcastViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     
     @IBOutlet weak var podcastURLTextField: NSTextField!
+    @IBOutlet weak var tableView: NSTableView!
+ 
+    
+    var podcasts : [Podcast] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        getPodcasts()
+        podcastURLTextField.stringValue = "http://www.espn.com/espnradio/podcast/feeds/itunes/podCast?id=2406595"
+    }
+    
+    func getPodcasts() {
+        if let context = (NSApplication.shared().delegate as? AppDelegate)?.managedObjectContext {
+            let fetchy = Podcast.fetchRequest() as NSFetchRequest<Podcast>
+            fetchy.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+            
+            do {
+                podcasts = try context.fetch(fetchy)
+                
+                print(podcasts)
+            } catch {}
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+            
+        }
     }
     
     @IBAction func addPodcastClicked(_ sender: Any) {
@@ -28,7 +54,19 @@ class PodcastViewController: NSViewController {
                     
                     if data != nil {
                         let parser = Parser()
-                        let info = parser.getPodcastMetaData(data: data!) 
+                        let info = parser.getPodcastMetaData(data: data!)
+                        
+                        if let context = (NSApplication.shared().delegate as? AppDelegate)?.managedObjectContext {
+                            let podcast = Podcast(context: context)
+
+                            podcast.rssURL = self.podcastURLTextField.stringValue
+                            podcast.imageURL = info.imageURL
+                            podcast.title = info.title
+                            
+                            (NSApplication.shared().delegate as? AppDelegate)?.saveAction(nil)
+                            
+                            self.getPodcasts()
+                        }
                         
                     }
                     
@@ -40,4 +78,24 @@ class PodcastViewController: NSViewController {
             
         }
     }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return podcasts.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = tableView.make(withIdentifier: "podcastcell", owner: self) as? NSTableCellView
+        
+        let podcast = podcasts[row]
+        
+        if podcast.title != nil {
+        
+            cell?.textField?.stringValue = podcast.title!
+        } else {
+            cell?.textField?.stringValue = "UNKNOWN TITLE"
+        }
+        
+        return cell
+    }
+    
 }
